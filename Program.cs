263d9cedbaf2186bd5292632df1cd52c4b165029
Shubham Mule <shubhambmule@gmail.com>
2021-10-21 23:20:15 +0530

    Welcome to generics

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsMaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to finding maximum number using generics");
+        }
+    }
+}
